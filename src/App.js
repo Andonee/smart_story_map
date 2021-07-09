44fbece839368201dc7d4db71ca110d20fca868a
mpoprawa <m.poprawa@smartgeomatic.pl>
2021-07-09 12:18:25 +0200

    add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import Auth from './pages/Auth'
 import Account from './pages/Account'
 import StoryMap from './pages/StoryMap'
@@ -10,22 +10,24 @@ function App() {
 	const authContext = useContext(AuthContext)
 	return (
 		<div className='App'>
-			<Switch>
-				<Route path='/story-account/' exact>
-					<Auth />
-				</Route>
-				{authContext.isLoggedIn && (
-					<Route path='/story-account/maps/:user'>
-						<Account />
+			<BrowserRouter>
+				<Switch>
+					<Route path='/story-account/' exact>
+						<Auth />
 					</Route>
-				)}
-				<Route path='/map/:user/:id'>
-					<StoryMap />
-				</Route>
-				<Route path='/story-account/*'>
-					<Redirect to='/story-account/' />
-				</Route>
-			</Switch>
+					{authContext.isLoggedIn && (
+						<Route path='/story-account/maps/:user'>
+							<Account />
+						</Route>
+					)}
+					<Route path='/map/:user/:id'>
+						<StoryMap />
+					</Route>
+					<Route path='/story-account/*'>
+						<Redirect to='/story-account/' />
+					</Route>
+				</Switch>
+			</BrowserRouter>
 		</div>
 	)
 }
