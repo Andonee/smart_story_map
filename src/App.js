394cff2704703c5221df0eb5a5999132a9e8d70d
mpoprawa <m.poprawa@smartgeomatic.pl>
2021-07-15 13:35:23 +0200

    change main route

--- src/App.js
+++ src/App.js
@@ -12,19 +12,19 @@ function App() {
 		<div className='App'>
 			<BrowserRouter basename={process.env.PUBLIC_URL}>
 				<Switch>
-					<Route path='/story-account/' exact>
+					<Route path='/smart-story-map/' exact>
 						<Auth />
 					</Route>
 					{authContext.isLoggedIn && (
-						<Route path='/story-account/maps/:user'>
+						<Route path='/smart-story-map/maps/:user'>
 							<Account />
 						</Route>
 					)}
 					<Route path='/map/:user/:id'>
 						<StoryMap />
 					</Route>
-					<Route path='/*'>
-						<Redirect to='/story-account/' />
+					<Route path='/smart-story-map/*'>
+						<Redirect to='/smart-story-map/' />
 					</Route>
 				</Switch>
 			</BrowserRouter>
